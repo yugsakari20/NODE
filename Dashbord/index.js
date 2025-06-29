23045f0470cadcb188b445489506e2f13a5bb470
yugsakari20 <yugsakariya976@gmail.com>
2025-06-29 18:01:24 +0530

    29/06/25

---
+++ Dashbord/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.redirect('/dashboard');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.post('/submit-form', (req, res) => {
+  const submittedData = req.body;
+  res.render('table', { submittedData });
+});
+
+app.get('/table', (req, res) => {
+  res.render('table', { submittedData: null });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
