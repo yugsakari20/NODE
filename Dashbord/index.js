1d0280d340ce7cdf98695e1cae9293b97f48898d
yugsakari20 <yugsakariya976@gmail.com>
2025-06-30 10:36:38 +0530

    29/06/25

--- Dashbord/index.js
+++ Dashbord/index.js
@@ -8,13 +8,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Routes
 app.get('/', (req, res) => {
   res.redirect('/dashboard');
 });
